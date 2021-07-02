c78abd00fdd2f8125ae34e7725fc1598a8cc5f5e
Luis Arce <luilliarcec@gmail.com>
2021-07-02 01:35:29 -0500

    Facade

---
+++ src/Facades/Table.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Luilliarcec\LaravelUsernameGenerator\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Luilliarcec\LaravelTable\Support\BladeTable;
+
+/**
+ * @method static BladeTable disableGlobalSearch()
+ * @method static BladeTable addColumn(string $key, string $label)
+ * @method static BladeTable addColumns(array $columns = [])
+ * @method static BladeTable addFilter(string $key, string $label, string $type, array $options = [])
+ * @method static BladeTable build()
+ *
+ * @see UsernameGenerator
+ */
+class Table extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return BladeTable::class;
+    }
+}
