94401465ead88a077be62ba9976882987bf44fdf
Luis Arce <luilliarcec@gmail.com>
2021-07-09 16:25:03 -0500

    SelectMultiple fix github tests

--- resources/views/components/tailwind/filters/select-multiple.blade.php
+++ resources/views/components/tailwind/filters/select-multiple.blade.php
@@ -1,8 +1,7 @@
 <select
     class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-700 block w-full pr-10 text-sm rounded-md filter-select-multiple"
     name="{{ $name }}"
-    multiple
->
+    multiple>
     @foreach($filter->options as $key => $option)
         <option
             value="{{ $key }}"
