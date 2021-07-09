ccc2c4840c08c25c672e1ac8b4afde2bcea81111
Luis Arce <luilliarcec@gmail.com>
2021-07-09 16:08:39 -0500

    Date Range fix github tests

--- resources/views/components/bootstrap5/filters/date-range.blade.php
+++ resources/views/components/bootstrap5/filters/date-range.blade.php
@@ -2,5 +2,4 @@
     type="text"
     class="form-control filter-date-range"
     name="{{ $name }}"
-    value="{{ $value }}"
->
+    value="{{ $value }}">
--- resources/views/components/tailwind/filters/date-range.blade.php
+++ resources/views/components/tailwind/filters/date-range.blade.php
@@ -2,5 +2,4 @@
     type="text"
     class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-700 block w-full pr-10 text-sm rounded-md filter-date-range"
     name="{{ $name }}"
-    value="{{ $value }}"
->
+    value="{{ $value }}">
