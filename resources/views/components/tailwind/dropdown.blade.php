6741f3775bbaabaaaec1d9710457279abf1da26a
Luis Arce <luilliarcec@gmail.com>
2021-08-05 18:46:31 -0500

    Add width prop to dropdown

--- resources/views/components/tailwind/dropdown.blade.php
+++ resources/views/components/tailwind/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right'])
+@props(['align' => 'right', 'width' => 'w-64'])
 
 @php
     switch ($align) {
@@ -32,7 +32,7 @@
          x-transition:leave="transition ease-in duration-75"
          x-transition:leave-start="transform opacity-100 scale-100"
          x-transition:leave-end="transform opacity-0 scale-95"
-         class="absolute z-50 mt-2 w-64 rounded-md shadow-lg {{ $alignmentClasses }}"
+         class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
          style="display: none;">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 bg-white">
             {{ $content }}
