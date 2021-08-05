90d8530037460dea87c0d0e83966d61c7f2ed67e
Luis Arce <luilliarcec@gmail.com>
2021-08-05 18:51:31 -0500

    Remove unnecesary scripts for tw theme

--- resources/views/components/tailwind/scripts.blade.php
+++ resources/views/components/tailwind/scripts.blade.php
@@ -1,16 +1,1 @@
-<script>
-    function dropdown(event, dropdownId, placement = 'bottom-start') {
-        let element = event.target;
 
-        while (element.nodeName !== "BUTTON") {
-            element = element.parentNode;
-        }
-
-        let popper = Popper.createPopper(element, document.getElementById(dropdownId), {
-            placement: placement
-        });
-
-        document.getElementById(dropdownId).classList.toggle("hidden");
-        document.getElementById(dropdownId).classList.toggle("block");
-    }
-</script>
