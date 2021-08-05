17998b98a624f568ed8e93c47c8967cf128119f4
Luis Arce <luilliarcec@gmail.com>
2021-08-05 18:48:16 -0500

    Refactor tw filters

--- resources/views/components/tailwind/filters.blade.php
+++ resources/views/components/tailwind/filters.blade.php
@@ -1,54 +1,46 @@
-<div>
-    <div class="relative">
-        <button
-            type="button"
-            onclick="dropdown(event, 'filters-dropdown')"
-            {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm hover:shadow font-semibold rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-100 disabled:opacity-50 active:bg-white leading-5 transition']) }}
-            aria-expanded="false"
-        >
-            <x-table::icons.filters class="text-gray-400"/>
-        </button>
+<x-table::tailwind.dropdown align="left" {{ $attributes }}>
+    <x-slot name="trigger">
+        <x-table::icons.filters class="text-gray-400"/>
+    </x-slot>
 
-        <div class="absolute z-10">
-            <div id="filters-dropdown"
-                 class="hidden mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                @foreach($filters as $filter)
-                    <div>
-                        <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
-                            {{ $filter->label }}
-                        </h3>
-
-                        <div class="p-2">
-                            @if($filter->type === \Luilliarcec\LaravelTable\Support\Filter::SELECT)
-                                <x-table::filters.select :filter="$filter"/>
-                            @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::TEXT)
-                                <x-table::filters.text :filter="$filter"/>
-                            @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::DATE)
-                                <x-table::filters.date :filter="$filter"/>
-                            @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::DATE_RANGE)
-                                <x-table::filters.date-range :filter="$filter"/>
-                            @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::CHECKBOX)
-                                <x-table::filters.checkbox :filter="$filter"/>
-                            @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::SELECT_MULTIPLE)
-                                <x-table::filters.select-multiple :filter="$filter"/>
-                            @endif
-                        </div>
-                    </div>
-                @endforeach
-
-                <div>
+    <x-slot name="content">
+        <ul>
+            @foreach($filters as $filter)
+                <li>
                     <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
-                        {{ __('Filters') }}
+                        {{ $filter->label }}
                     </h3>
 
                     <div class="p-2">
-                        <a class="text-gray-400 decoration-none px-2"
-                           href="{{ request()->url() }}">
-                            {{ __('Clear filters') }}
-                        </a>
+                        @if($filter->type === \Luilliarcec\LaravelTable\Support\Filter::SELECT)
+                            <x-table::filters.select :filter="$filter"/>
+                        @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::TEXT)
+                            <x-table::filters.text :filter="$filter"/>
+                        @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::DATE)
+                            <x-table::filters.date :filter="$filter"/>
+                        @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::DATE_RANGE)
+                            <x-table::filters.date-range :filter="$filter"/>
+                        @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::CHECKBOX)
+                            <x-table::filters.checkbox :filter="$filter"/>
+                        @elseif($filter->type === \Luilliarcec\LaravelTable\Support\Filter::SELECT_MULTIPLE)
+                            <x-table::filters.select-multiple :filter="$filter"/>
+                        @endif
                     </div>
+                </li>
+            @endforeach
+
+            <li>
+                <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
+                    {{ __('Filters') }}
+                </h3>
+
+                <div class="p-2">
+                    <a class="text-gray-400 decoration-none px-2"
+                       href="{{ request()->url() }}">
+                        {{ __('Clear filters') }}
+                    </a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+            </li>
+        </ul>
+    </x-slot>
+</x-table::tailwind.dropdown>
--- tests/Units/Views/FiltersTest.php
+++ tests/Units/Views/FiltersTest.php
@@ -21,47 +21,38 @@ class FiltersTest extends TestCase
 
         $view
             ->assertRender('
-                <div>
-                    <div class="relative">
-                        <button type="button" onclick="dropdown(event, \'filters-dropdown\')" class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm hover:shadow font-semibold rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-100 disabled:opacity-50 active:bg-white leading-5 transition" aria-expanded="false" >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="text-white text-gray-400"
-                                style="height: 1.25rem; width: 1.25rem"
-                                viewBox="0 0 20 20"
-                                fill="currentColor">
-                                <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                        <div class="absolute z-10">
-                            <div id="filters-dropdown" class="hidden mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                                <div>
+                <div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+                    <button type="button" @click="open = !open" class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm hover:shadow font-semibold rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-100 disabled:opacity-50 active:bg-white leading-5 transition" :aria-expanded="open" >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-white text-gray-400" style="height: 1.25rem; width: 1.25rem" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute z-50 mt-2 w-64 rounded-md shadow-lg origin-top-left left-0" style="display: none;">
+                        <div class="rounded-md ring-1 ring-black ring-opacity-5 bg-white">
+                            <ul>
+                                <li>
                                     <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
                                         Name
                                     </h3>
                                     <div class="p-2">
                                         <input type="text" class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-700 block w-full pr-10 text-sm rounded-md filter-text" name="filter[name]" value="">
                                     </div>
-                                </div>
-                                <div>
+                                </li>
+                                <li>
                                     <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
                                         Created At
                                     </h3>
                                     <div class="p-2">
                                         <input type="date" class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-700 block w-full pr-10 text-sm rounded-md filter-date" name="filter[created_at]" value="">
                                     </div>
-                                </div>
-                                <div>
+                                </li>
+                                <li>
                                     <h3 class="text-xs uppercase tracking-wide bg-gray-100 p-3">
                                         Filters
                                     </h3>
-                                    <div class="p-2">
-                                        <a class="text-gray-400 decoration-none px-2" href="http://localhost">
-                                            Clear filters
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                                    <div class="p-2"> <a class="text-gray-400 decoration-none px-2" href="http://localhost"> Clear filters </a> </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
