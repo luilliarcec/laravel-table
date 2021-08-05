329ca10f6a316eba43fd6adb34443a2b8cc2420f
Luis Arce <luilliarcec@gmail.com>
2021-08-05 18:33:42 -0500

    Create dropdown component for tailwind theme

---
+++ resources/views/components/tailwind/dropdown.blade.php
@@ -0,0 +1,24 @@
+<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+    <button
+        type="button"
+        @click="open = !open"
+        {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm hover:shadow font-semibold rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-100 disabled:opacity-50 active:bg-white leading-5 transition']) }}
+        :aria-expanded="open"
+    >
+        {{ $trigger }}
+    </button>
+
+    <div x-show="open"
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="transform opacity-0 scale-95"
+         x-transition:enter-end="transform opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="transform opacity-100 scale-100"
+         x-transition:leave-end="transform opacity-0 scale-95"
+         class="absolute z-50 mt-2 w-64 rounded-md shadow-lg origin-top-right right-0"
+         style="display: none;">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
+            {{ $content }}
+        </div>
+    </div>
+</div>
