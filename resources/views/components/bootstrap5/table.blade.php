c9ab8cb85611542d60f66a2d36e465671f264459
Luis Arce <luilliarcec@gmail.com>
2021-08-07 10:06:41 -0500

    Add action button component

--- resources/views/components/bootstrap5/table.blade.php
+++ resources/views/components/bootstrap5/table.blade.php
@@ -33,11 +33,7 @@
                 @isset($actionButton)
                     {{ $actionButton }}
                 @else
-                    <div class="col-4 col-md-2 mt-2">
-                        <button type="submit" class="btn btn-dark w-100">
-                            <x-table::icons.trigger/>
-                        </button>
-                    </div>
+                    <x-table::action-button/>
                 @endisset
             @endif
         </div>
---
+++ resources/views/components/tailwind/action-button.blade.php
@@ -0,0 +1,4 @@
+<button type="submit"
+        class="inline-flex items-center mt-2 py-2 px-4 border border-gray-300 shadow-sm hover:shadow font-semibold rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-100 disabled:opacity-50 active:bg-white leading-5 transition">
+    <x-table::icons.trigger class="text-gray-400"/>
+</button>
