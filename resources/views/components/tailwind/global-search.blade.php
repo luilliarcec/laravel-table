59629daeeb3e5bb2ccefe7f4c36bcc639d764a29
Luis Arce <luilliarcec@gmail.com>
2021-08-07 10:00:57 -0500

    Redesign global search

--- resources/views/components/tailwind/global-search.blade.php
+++ resources/views/components/tailwind/global-search.blade.php
@@ -1,12 +1,10 @@
 <div class="relative">
     <input
-        {{ $attributes->merge(['class' => 'border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-900 block w-full pr-10 text-sm rounded-md']) }}
+        {{ $attributes->merge(['class' => 'border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-900 block w-full text-sm rounded-md']) }}
         name="filter[global]"
         type="search"
         placeholder="Search..."
         aria-label="Global search"
         value="{{ request('filter.global') }}"
     >
-
-    <x-table::icons.search class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none text-gray-400"/>
 </div>
--- tests/Units/Views/GlobalSearchTest.php
+++ tests/Units/Views/GlobalSearchTest.php
@@ -18,19 +18,13 @@ class GlobalSearchTest extends TestCase
             ->assertRender('
                 <div class="relative">
                     <input
-                        class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-900 block w-full pr-10 text-sm rounded-md"
+                        class="border-gray-300 focus:ring-1 focus:ring-blue-900 focus:border-blue-900 block w-full text-sm rounded-md"
                         name="filter[global]"
                         type="search"
                         placeholder="Search..."
                         aria-label="Global search"
                         value=""
                     >
-
-                    <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none text-gray-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-400" style="height: 1.25rem; width: 1.25rem" viewBox="0 0 20 20" fill="currentColor" >
-                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
                 </div>
             ');
     }
