5ba3a4a78743c9280e7461aa7dae683e0dd0ad02
Luis Arce <luilliarcec@gmail.com>
2022-06-21 13:01:15 -0500

    Table translations

---
+++ resources/lang/en/table.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'empty' => [
+        'heading' => 'No results match the given criteria.',
+    ],
+
+    'fields' => [
+        'search_query' => [
+            'label' => 'Search',
+            'placeholder' => 'Search',
+        ],
+    ],
+
+    'pagination' => [
+        'label' => 'Pagination Navigation',
+        'overview' => 'Showing :first to :last of :total results',
+
+        'fields' => [
+            'records_per_page' => [
+                'label' => '',
+            ],
+        ],
+
+        'buttons' => [
+            'go_to_page' => [
+                'label' => 'Go to page :page',
+            ],
+
+            'next' => [
+                'label' => '',
+            ],
+
+            'previous' => [
+                'label' => '',
+            ],
+        ],
+    ],
+
+    'filters' => [
+        'buttons' => [
+            'reset' => [
+                'label' => 'Reset filters',
+            ],
+            'close' => [
+                'label' => 'Close',
+            ],
+        ],
+
+        'multi_select' => [
+            'placeholder' => 'All',
+        ],
+
+        'select' => [
+            'placeholder' => 'All',
+        ],
+    ],
+];
---
+++ resources/lang/es/table.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'empty' => [
+        'heading' => 'Ningún resultado coincide con los criterios dados.',
+    ],
+
+    'fields' => [
+        'search_query' => [
+            'label' => 'Buscar',
+            'placeholder' => 'Buscar',
+        ],
+    ],
+
+    'pagination' => [
+        'label' => 'Navegación de paginación',
+        'overview' => 'Mostrando :first a :last de :total resultados',
+
+        'fields' => [
+            'records_per_page' => [
+                'label' => '',
+            ],
+        ],
+
+        'buttons' => [
+            'go_to_page' => [
+                'label' => 'Ir a la página :page',
+            ],
+
+            'next' => [
+                'label' => '',
+            ],
+
+            'previous' => [
+                'label' => '',
+            ],
+        ],
+    ],
+
+    'filters' => [
+        'buttons' => [
+            'reset' => [
+                'label' => 'Resetear los filtros',
+            ],
+            'close' => [
+                'label' => 'Cerrar',
+            ],
+        ],
+
+        'multi_select' => [
+            'placeholder' => 'Todos',
+        ],
+
+        'select' => [
+            'placeholder' => 'Todos',
+        ],
+    ],
+];
