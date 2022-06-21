809287aa5d0f2ff7c288b434e934be755781a951
Luis Arce <luilliarcec@gmail.com>
2022-06-21 13:01:33 -0500

    Table config

--- config/tables.php
+++ config/tables.php
@@ -2,4 +2,9 @@
 
 return [
     'dark_mode' => false,
+
+    'layout' => [
+        'action_alignment' => 'right',
+        'default_pagination_view' => 'tables::components.pagination.index'
+    ],
 ];
