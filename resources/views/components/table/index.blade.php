9ee6e187a7b72f46618ab19aaa7798ca1ac2066c
Luis Arce <luilliarcec@gmail.com>
2022-06-21 04:17:54 -0500

    Table component view

---
+++ resources/views/components/table/index.blade.php
@@ -0,0 +1,35 @@
+@props([
+    'footer' => null,
+    'header' => null,
+])
+
+<table {{ $attributes->class([
+    'w-full text-left rtl:text-right divide-y table-auto blade-tables-table',
+    'dark:divide-gray-700' => config('tables.dark_mode'),
+]) }}>
+    @if ($header)
+        <thead>
+        <tr @class([
+                'bg-gray-50',
+                'dark:bg-gray-500/10' => config('tables.dark_mode'),
+            ])>
+            {{ $header }}
+        </tr>
+        </thead>
+    @endif
+
+    <tbody @class([
+        'divide-y whitespace-nowrap',
+        'dark:divide-gray-700' => config('tables.dark_mode'),
+    ])>
+    {{ $slot }}
+    </tbody>
+
+    @if ($footer)
+        <tfoot>
+        <tr class="bg-gray-50">
+            {{ $footer }}
+        </tr>
+        </tfoot>
+    @endif
+</table>
