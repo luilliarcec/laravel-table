72808e3c97f5201907576f791a838d0c985054b8
Luis Arce <luilliarcec@gmail.com>
2022-06-22 01:36:04 -0500

    FilterComponent

---
+++ src/Filters/Concerns/HasView.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Luilliarcec\LaravelTable\Filters\Concerns;
+
+trait HasView
+{
+    protected string $view;
+
+    public function view(string $view): static
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+    public function getView(): string
+    {
+        return $this->view;
+    }
+}
---
+++ src/Filters/Filter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Luilliarcec\LaravelTable\Filters;
+
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Traits;
+use Illuminate\View\Component;
+
+class Filter extends Component implements Htmlable
+{
+    use Traits\Conditionable;
+    use Traits\Macroable;
+    use Traits\Tappable;
+    use Concerns\HasView;
+
+    public function toHtml(): string
+    {
+        return $this->render()->render();
+    }
+
+    public function render(): View
+    {
+        $data = array_merge($this->data(), [
+            'column' => $this,
+        ]);
+
+        return view($this->getView(), $data);
+    }
+}
