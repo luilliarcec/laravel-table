1df914a47e4e8148e2887395a1a6bb412aeeff82
Luis Arce <luilliarcec@gmail.com>
2022-06-23 17:48:17 -0500

    ExtraInputAttributes

---
+++ src/Filters/Concerns/HasExtraInputAttributes.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Luilliarcec\LaravelTable\Filters\Concerns;
+
+use Closure;
+use Illuminate\View\ComponentAttributeBag;
+
+trait HasExtraInputAttributes
+{
+    protected array|Closure $extraInputAttributes = [];
+
+    public function extraInputAttributes(array|Closure $attributes): static
+    {
+        $this->extraInputAttributes = $attributes;
+
+        return $this;
+    }
+
+    public function getExtraInputAttributes(): array
+    {
+        return $this->evaluate($this->extraInputAttributes);
+    }
+
+    public function getExtraInputAttributeBag(): ComponentAttributeBag
+    {
+        return new ComponentAttributeBag($this->getExtraInputAttributes());
+    }
+}
