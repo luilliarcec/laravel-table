5d8d9f3b144940616f98a70fd14c5ed6b3c72788
Luis Arce <luilliarcec@gmail.com>
2022-06-23 17:37:36 -0500

    TextFilter view WIP

--- resources/views/filters/text.blade.php
+++ resources/views/filters/text.blade.php
@@ -1,3 +1,11 @@
+@php
+    $datalistOptions = $getDatalistOptions();
+    $tableName = $getTable()->getName();
+    $sideLabelClasses = [
+        'whitespace-nowrap group-focus-within:text-primary-500',
+    ];
+@endphp
+
 <x-tables::fields
     :id="$getId()"
     :label="$getLabel()"
@@ -6,5 +14,30 @@
     :hint="$getHint()"
     :hint-icon="$getHintIcon()"
 >
-    Text Filter
+    <div {{ $attributes->merge($getExtraAttributes())->class(['flex items-center space-x-1 rtl:space-x-reverse group']) }}>
+        @if ($prefix = $getPrefixLabel())
+            <span @class($sideLabelClasses)>
+                {{ $prefix }}
+            </span>
+        @endif
+
+        <div class="flex-1">
+            <input type="text"
+            >
+        </div>
+
+        @if ($suffix = $getSuffixLabel())
+            <span @class($sideLabelClasses)>
+                {{ $suffix }}
+            </span>
+        @endif
+    </div>
+
+    @if ($datalistOptions)
+        <datalist id="{{ $getId() }}-list">
+            @foreach ($datalistOptions as $option)
+                <option value="{{ $option }}"/>
+            @endforeach
+        </datalist>
+    @endif
 </x-tables::fields>
