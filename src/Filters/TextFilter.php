95b04a3696dfc45921995b3254507e90f4af9e24
Luis Arce <luilliarcec@gmail.com>
2022-06-23 17:36:28 -0500

    TextFilter DataList

--- src/Filters/TextFilter.php
+++ src/Filters/TextFilter.php
@@ -2,7 +2,30 @@
 
 namespace Luilliarcec\LaravelTable\Filters;
 
+use Closure;
+use Illuminate\Contracts\Support\Arrayable;
+
 class TextFilter extends Filter
 {
     protected string $view = 'tables::filters.text';
+
+    protected array|Arrayable|Closure|null $datalistOptions = null;
+
+    public function datalist(array|Arrayable|Closure|null $options): static
+    {
+        $this->datalistOptions = $options;
+
+        return $this;
+    }
+
+    public function getDatalistOptions(): ?array
+    {
+        $options = $this->evaluate($this->datalistOptions);
+
+        if ($options instanceof Arrayable) {
+            $options = $options->toArray();
+        }
+
+        return $options;
+    }
 }
