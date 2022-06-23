6a56d43eb277991251fa0d9d26969c9eb1b73514
Luis Arce <luilliarcec@gmail.com>
2022-06-23 15:07:09 -0500

    WIP

--- resources/views/filters/text.blade.php
+++ resources/views/filters/text.blade.php
@@ -1,1 +1,10 @@
-Text Filter
+<x-tables::fields
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :hint-icon="$getHintIcon()"
+>
+    Text Filter
+</x-tables::fields>
