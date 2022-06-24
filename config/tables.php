8456aeb40d3e8435349c8d66f8f39f104ee4083e
Luis Arce <luilliarcec@gmail.com>
2022-06-24 04:58:30 -0500

    WIP

--- config/tables.php
+++ config/tables.php
@@ -3,6 +3,8 @@
 return [
     'dark_mode' => false,
 
+    'default_filesystem_disk' => env('TABLES_FILESYSTEM_DRIVER', 'public'),
+
     'layout' => [
         'action_alignment' => 'right',
         'default_pagination_view' => 'tables::components.pagination.index'
