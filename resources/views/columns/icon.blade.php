222983aa24f2767071f4c30ccef7a8c902b46e00
Luis Arce <luilliarcec@gmail.com>
2022-06-24 04:43:56 -0500

    IconColumn.php

---
+++ resources/views/columns/icon.blade.php
@@ -0,0 +1,19 @@
+@php
+    $stateColor = match ($getStateColor()) {
+        'danger' => 'text-danger-500',
+        'primary' => 'text-primary-500',
+        'success' => 'text-success-500',
+        'warning' => 'text-warning-500',
+        null => \Illuminate\Support\Arr::toCssClasses(['text-gray-700', 'dark:text-gray-200' => config('tables.dark_mode'),]),
+        default => $getStateColor(),
+    };
+@endphp
+
+<div {{ $attributes->merge($getExtraAttributes())->class(['px-4 py-3']) }}>
+    @if ($getStateIcon())
+        <x-dynamic-component
+            :component="$getStateIcon()"
+            :class="'w-6 h-6 ' . $stateColor"
+        />
+    @endif
+</div>
