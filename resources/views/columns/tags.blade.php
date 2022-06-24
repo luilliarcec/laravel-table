4163679cb1c286b745b73f703891f095b8d08c21
Luis Arce <luilliarcec@gmail.com>
2022-06-24 04:08:01 -0500

    TagsColumn.php

---
+++ resources/views/columns/tags.blade.php
@@ -0,0 +1,22 @@
+@php
+    $state = $getTags();
+@endphp
+
+<div {{ $attributes->merge($getExtraAttributes())->class([
+    'px-4 py-3 flex flex-wrap gap-1',
+    match ($getAlignment()) {
+        'left' => 'justify-start',
+        'center' => 'justify-center',
+        'right' => 'justify-end',
+        default => null,
+    },
+]) }}>
+    @foreach ($getTags() as $tag)
+        <span @class([
+            'inline-flex items-center justify-center min-h-6 px-2 py-0.5 text-sm font-medium tracking-tight rounded-xl text-primary-700 bg-primary-500/10 whitespace-normal',
+            'dark:text-primary-500' => config('tables.dark_mode'),
+        ])>
+            {{ $tag }}
+        </span>
+    @endforeach
+</div>
---
+++ src/Columns/TagsColumn.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Luilliarcec\LaravelTable\Columns;
+
+use Closure;
+
+class TagsColumn extends Column
+{
+    protected string $view = 'tables::columns.tags';
+
+    protected string|Closure|null $separator = null;
+
+    public function getTags(): array
+    {
+        $tags = $this->getState();
+
+        if (is_array($tags)) {
+            return $tags;
+        }
+
+        if (!($separator = $this->getSeparator())) {
+            return [];
+        }
+
+        $tags = explode($separator, $tags);
+
+        if (count($tags) === 1 && blank($tags[0])) {
+            $tags = [];
+        }
+
+        return $tags;
+    }
+
+    public function separator(string|Closure|null $separator = ','): static
+    {
+        $this->separator = $separator;
+
+        return $this;
+    }
+
+    public function getSeparator(): ?string
+    {
+        return $this->evaluate($this->separator);
+    }
+}
