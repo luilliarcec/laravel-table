b198265f49e03a6331d16abd2bcd0ceed52c664d
Luis Arce <luilliarcec@gmail.com>
2022-06-23 22:34:35 -0500

    Column tables formats

--- config/tables.php
+++ config/tables.php
@@ -7,4 +7,10 @@ return [
         'action_alignment' => 'right',
         'default_pagination_view' => 'tables::components.pagination.index'
     ],
+
+    'formats' => [
+        'date' => 'M j, Y',
+        'datetime' => 'M j, Y H:i:s',
+        'time' => 'H:i:s',
+    ],
 ];
