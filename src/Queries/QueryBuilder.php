0ce6b0883e0fbfa5b6390b292c00adbe83da39ae
Luis Arce <luilliarcec@gmail.com>
2022-06-28 10:44:19 -0500

    WIP

--- src/Queries/QueryBuilder.php
+++ src/Queries/QueryBuilder.php
@@ -3,11 +3,31 @@
 namespace Luilliarcec\LaravelTable\Queries;
 
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
+use Luilliarcec\LaravelTable\Queries\Filters\SearchFilter;
 use Luilliarcec\LaravelTable\Table;
+use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder as BaseQueryBuilder;
 
-class QueryBuilder extends BaseQueryBuilder
+abstract class QueryBuilder extends BaseQueryBuilder
 {
+    protected Table $table;
+
+    public function __construct($subject, ?Request $request = null)
+    {
+        parent::__construct($subject, $request);
+
+        $this->table = Table::make($this->tableName())
+            ->columns($this->tableColumns())
+            ->filters($this->tableFilters())
+            ->actions($this->tableActions())
+            ->emptyStateActions($this->tableEmptyStateActions());
+
+        $this
+            ->allowedSorts($this->table->getSortableColumns())
+            ->allowedFilters(AllowedFilter::custom('search', new SearchFilter($this->table->getSearchableColumns())));
+    }
+
     public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null): LengthAwarePaginator
     {
         $perPage = $this->request->query('per_page', $perPage);
@@ -19,15 +39,30 @@ class QueryBuilder extends BaseQueryBuilder
         return $paginator;
     }
 
+    public function getTable(): Table
+    {
+        return $this->table;
+    }
+
     public function toPaginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null): Table
     {
-        return $this->table()
+        return $this->table
             ->records($this->paginate($perPage, $columns, $pageName, $page));
     }
 
     public function toCollection($columns = ['*']): Table
     {
-        return $this->table()
+        return $this->table
             ->records($this->get($columns));
     }
+
+    protected abstract function tableName(): string;
+
+    protected abstract function tableColumns(): array;
+
+    protected abstract function tableFilters(): array;
+
+    protected abstract function tableActions(): array;
+
+    protected abstract function tableEmptyStateActions(): array;
 }
